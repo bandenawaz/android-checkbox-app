f0ce397e944a911e6d03d235cfc3c47af390d928
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-15 12:07:01 +0530

    Fronted Commit

---
+++ app/src/main/java/com/sveri/checkboxapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sveri.checkboxapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
